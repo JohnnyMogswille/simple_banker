903c3f2e319ef8490328864cb35f1bd196cd8e47
JohnnyMogswille <eugene.mogush@gmail.com>
2024-06-09 22:51:58 +0500

    Страница помощи

---
+++ src/components/questions/QuestionItem.vue
@@ -0,0 +1,23 @@
+<template lang="">
+	<div class="question">
+		<div class="question-title" @click="isOpen = !isOpen">
+			{{ question.title }}
+		</div>
+		<div v-if="isOpen" class="question-body">
+			{{ question.text }}
+		</div>
+	</div>
+</template>
+<script>
+import { ref } from 'vue'
+
+export default {
+	props: ['question'],
+	setup() {
+		return {
+			isOpen: ref(false)
+		}
+	}
+}
+</script>
+<style lang=""></style>
--- src/views/HelpView.vue
+++ src/views/HelpView.vue
@@ -1,5 +1,33 @@
+<!-- eslint-disable vue/html-indent -->
 <template>
-	<div class="about">
-		<h1>This is an about page</h1>
-	</div>
+	<app-page title="Помощь">
+		<question-item
+			v-for="(item, index) in questions"
+			:key="index"
+			:question="item"
+		/>
+	</app-page>
 </template>
+
+<script>
+import AppPage from '@/components/ui/AppPage.vue'
+import QuestionItem from '@/components/questions/QuestionItem.vue'
+
+export default {
+	components: { AppPage, QuestionItem },
+	setup() {
+		const questions = [
+			{
+				title: 'Что такое Vue?',
+				text: 'Vue.js — это прогрессивный фреймворк для разработки пользовательских интерфейсов и одностраничных веб-приложений на языке JavaScript'
+			},
+			{
+				title: 'Что такое Vuex?',
+				text: 'Vuex — это стандартное хранилище состояний для Vue.js, которое позволяет нам хранить данные в виде объектов'
+			}
+		]
+
+		return { questions }
+	}
+}
+</script>
